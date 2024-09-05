b80478271097e73167be3a5e2b83337217056cba
Chaitanya Gurujala <chaitanya.gurujala@gmail.com>
2024-09-05 18:49:55 +0000

    9.5.2024 - afternoon

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["case1fe/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"case1fe/test/unit/controller/S1.controller"
+], function () {
+	"use strict";
+});
